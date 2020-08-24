d533e8f6edd5b0488473542db8719377d906dd0b
ShaharEli <shahar.e3@gmail.com>
2020-08-24 11:53:08 +0300

    initial import

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
-import React, {useEffect, useState} from 'react';
-import logo from './logo.svg';
-import Ticket from './components/Ticket'
+import React from 'react';
+
+// import Ticket from "./components/Ticket";
 import './App.css';
 
 function App() {
   return (
-    <main>
-    </main>
+    <main />
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -1,4 +1,25 @@
+const fs = require('fs').promises;
+const express = require('express');
 const app = require('./app');
-const port = 8080;
 
+const port = 8080;
+app.use(express.json());
+app.use(express.static('../client/build'));
+
+app.get('/api/v1/records', async (req, res) => {
+  const data = await fs.readFile(path);
+  const data1 = JSON.parse(data);
+  res.send(data1);
+});
+
+app.post('/api/v1/record', async (req, res) => {
+  try {
+    const data = await fs.readFile(path);
+    let data1 = JSON.parse(data);
+    data1.push(req.body);
+    data1 = JSON.stringify(data1);
+    await fs.writeFile(path, data1);
+    res.send('done');
+  } catch (e) { console.log(e); }
+});
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
