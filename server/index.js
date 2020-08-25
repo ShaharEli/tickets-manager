612916cd7958d03b2f60f9305d226ec04faf1314
ShaharEli <shahar.e3@gmail.com>
2020-08-25 19:01:09 +0300

    better naming for the backend

--- server/index.js
+++ server/index.js
@@ -7,44 +7,44 @@ app.use(express.json());
 app.use(express.static('../client/build'));
 const path = './data.json';
 app.get('/api/tickets', async (req, res) => {
-  const data = await fs.readFile(path);
-  const data1 = JSON.parse(data);
+  const tickets = await fs.readFile(path);
+  const ticketsJson = JSON.parse(tickets);
   if (!req.query.searchText) {
-    res.send(data1);
+    res.send(ticketsJson);
   } else {
-    const data2 = data1.filter((item) => item.title.toLowerCase().includes(req.query.searchText.toLowerCase()));
-    res.send(data2);
+    const ticketsArray = ticketsJson.filter((item) => item.title.toLowerCase().includes(req.query.searchText.toLowerCase()));
+    res.send(ticketsArray);
   }
 });
 
 app.post('/api/tickets/:ticketId/done', async (req, res) => {
-  const data = await fs.readFile(path);
-  let data1 = JSON.parse(data);
+  const tickets = await fs.readFile(path);
+  let ticketsJson = JSON.parse(tickets);
   try {
-    const data2 = data1.map((item) => {
+    const ticketsArray = ticketsJson.map((item) => {
       if (item.id === req.params.ticketId) {
         item.done = true;
       }
       return item;
     });
-    data1 = JSON.stringify(data2);
-    await fs.writeFile(path, data1);
+    ticketsJson = JSON.stringify(ticketsArray);
+    await fs.writeFile(path, ticketsJson);
     res.send({ updated: true });
   } catch (e) { res.send({ updated: false }); }
 });
 
 app.post('/api/tickets/:ticketId/undone', async (req, res) => {
-  const data = await fs.readFile(path);
-  let data1 = JSON.parse(data);
+  const tickets = await fs.readFile(path);
+  let ticketsJson = JSON.parse(tickets);
   try {
-    const data2 = data1.map((item) => {
+    const ticketsArray = ticketsJson.map((item) => {
       if (item.id === req.params.ticketId) {
         item.done = false;
       }
       return item;
     });
-    data1 = JSON.stringify(data2);
-    await fs.writeFile(path, data1);
+    ticketsJson = JSON.stringify(ticketsArray);
+    await fs.writeFile(path, ticketsJson);
     res.send({ updated: true });
   } catch (e) { res.send({ updated: true }); }
 });
