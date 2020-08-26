e9a1f8a1806c2cd3c19155eb3170f07866b4ff68
ShaharEli <shahar.e3@gmail.com>
2020-08-26 19:54:50 +0300

    eslint check again

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,12 @@ import Search from './components/Search';
 import Header from './components/Header';
 
 function App() {
-  const [counter, setCounter] = useState(0); //counter of hidden tickets
-  const [tickets, setTickets] = useState([]); //list of tickets
-  const [call, setCall] = useState(0); //state that will be passed as a prop to restore tickets
-  const [scrollDisplay, setScrollDisplay] = useState('none'); //state that will be used to show scroll up button
-  const scrollFunction = () => { //changing the display of the scroll up button if the page scrolled down
+  const [counter, setCounter] = useState(0); // counter of hidden tickets
+  const [tickets, setTickets] = useState([]); // list of tickets
+  const [call, setCall] = useState(0); // state that will be passed as a prop to restore tickets
+  const [scrollDisplay, setScrollDisplay] = useState('none'); // state that will be used to show scroll up button
+  const scrollFunction = () => { /* changing the display of the scroll up button if the
+    page scrolled down */
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
       setScrollDisplay('block');
     } else {
@@ -18,29 +19,32 @@ function App() {
     }
   };
   window.onscroll = scrollFunction;
-  const scrollUp = () => { //if the scroll up button pressed the page will scroll up
+  const scrollUp = () => { // if the scroll up button pressed the page will scroll up
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
   };
-  useEffect(() => { //initial get request for all the tickets
+  useEffect(() => { // initial get request for all the tickets
     const fetch = async () => {
       const list = await axios.get('/api/tickets');
       setTickets(list.data);
     };
     fetch();
   }, []);
-  const search = async (title) => { //search function that will be passed as a prop to the input field
+  const search = async (title) => { /* search function that will be passed as a prop
+     to the input field */
     const data = await axios.get(`/api/tickets?searchText=${title}`);
     setTickets(data.data);
   };
-  const addCount = () => { //count function that will be used in the ticket componenet to update the count of hidden tickets
+  const addCount = () => { /* count function that will be used in the ticket componenet
+     to update the count of hidden tickets */
     setCounter((prev) => prev + 1);
   };
-  const restore = async () => { //restore function to display hidden tickets
+  const restore = async () => { // restore function to display hidden tickets
     setCall((prev) => prev + 1);
     setCounter(0);
   };
-  const hiddenItems = () => { //if there is hidden tickets displayng the count of them and button to restore them
+  const hiddenItems = () => { /* if there is hidden tickets displayng the count of
+    them and button to restore them */
     if (counter === 0) {
       return '';
     }
@@ -66,26 +70,26 @@ function App() {
       <div id="list">
         <h2>
           Showing&nbsp;
-          {tickets.length+" "}
-          
+          {`${tickets.length} `}
+
           results
           {hiddenItems()}
         </h2>
         {
-        tickets.map((ticket, index) => (
-          <Ticket
-            addCount={addCount}
-            call={call}
-            key={index}
-            id={ticket.id}
-            title={ticket.title}
-            content={ticket.content}
-            userEmail={ticket.userEmail}
-            creationTime={new Date(ticket.creationTime)} 
-            labels={ticket.labels ? ticket.labels : null}
-          />
-        ))
-      }
+          tickets.map((ticket) => (
+            <Ticket
+              addCount={addCount}
+              call={call}
+              key={ticket.id}
+              id={ticket.id}
+              title={ticket.title}
+              content={ticket.content}
+              userEmail={ticket.userEmail}
+              creationTime={new Date(ticket.creationTime)}
+              labels={ticket.labels ? ticket.labels : null}
+            />
+          ))
+        }
       </div>
     </main>
   );
--- client/src/components/Ticket.js
+++ client/src/components/Ticket.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import './Ticket.css';
-import ReactCardFlip from 'react-card-flip'; //comopnent that will flip the ticket
+import ReactCardFlip from 'react-card-flip'; // comopnent that will flip the ticket
 
 function Ticket(props) {
-  const [isFlipped,setIsFlipped] = useState(true); //state for flipping animation
-  const [contentClass, setContentClass] = useState('content'); //state for changing the content class name
-  const [display, setDisplay] = useState('flex'); //state for hidding and displaying the ticket
-  const [buttonDisplay, setButtonDisplay] = useState('none'); //state for hidding and displaying the hide button
-  const [classTicket, setClassTicket] = useState('ticket'); //state for changing the ticket class name
+  const [isFlipped, setIsFlipped] = useState(true); // state for flipping animation
+  const [contentClass, setContentClass] = useState('content'); // state for changing the content class name
+  const [display, setDisplay] = useState('flex'); // state for hidding and displaying the ticket
+  const [buttonDisplay, setButtonDisplay] = useState('none'); // state for hidding and displaying the hide button
+  const [classTicket, setClassTicket] = useState('ticket'); // state for changing the ticket class name
   // function to add zeros to the date if needed
-  function addZero(i) {  
+  function addZero(i) {
     if (i < 10) {
       i = `0${i}`;
     }
@@ -28,75 +28,81 @@ function Ticket(props) {
     today = `${mm}/${dd}/${yyyy}`;
     return `${today}  ${h}:${m}:${s}`;
   }
-  const see = (e) => { //function that will change the hide button inner text
+  const see = (e) => { // function that will change the hide button inner text
     if (contentClass === 'content') {
-      setContentClass('content2');  //this class not limited in lines
+      setContentClass('content2'); // this class not limited in lines
       e.target.innerText = 'show less';
     } else {
-      setContentClass('content'); //this class is limited for 4 lines
+      setContentClass('content'); // this class is limited for 4 lines
       e.target.innerText = 'show more...';
     }
   };
-  useEffect(()=>{ //flip animation in the first mount
-    setTimeout(()=>{
-      setIsFlipped(false)
-    },400)}
-  ,[])
-  const hide = () => { //hide function that will increase the hiddin tickets counter and do flip animation
-    setClassTicket("hiddenTicket1"); //class just for the animation
+  useEffect(() => { // flip animation in the first mount
+    setTimeout(() => {
+      setIsFlipped(false);
+    }, 400);
+  },
+  []);
+  const hide = () => { /* hide function that will increase the hiddin tickets
+    counter and do flip animation */
+    setClassTicket('hiddenTicket1'); // class just for the animation
     setIsFlipped(false);
     setIsFlipped(true);
     props.addCount();
-    setTimeout(()=>{
-    setClassTicket('hiddenTicket');
-    setDisplay('none');
-    },1000);
+    setTimeout(() => {
+      setClassTicket('hiddenTicket');
+      setDisplay('none');
+    }, 1000);
   };
-  const showButton = () => { 
+  const showButton = () => {
     setButtonDisplay('block');
   };
   const hideButton = () => {
     setButtonDisplay('none');
   };
-  useEffect(() => { //function that will be called if the restore button pressed and will change the ticket to be visible and do flip animation
+  useEffect(() => { /* function that will be called if the restore button pressed and
+    will change the ticket to be visible and do flip animation */
     setIsFlipped(false);
     setDisplay('flex');
     setClassTicket('ticket');
   }, [props.call]);
   return (
-    <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical"> 
-    <div style={{ display }} className={classTicket} onMouseOver={showButton} onMouseOut={hideButton}>
-      <div className="titleAndButton">
-        <div>
-          <p className="title">{props.title}</p>
+    <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
+      <div
+        style={{ display }}
+        className={classTicket}
+        onMouseOver={showButton}
+        onMouseOut={hideButton}
+      >
+        <div className="titleAndButton">
+          <div>
+            <p className="title">{props.title}</p>
+          </div>
+          <div>
+            <button style={{ display: buttonDisplay }} onClick={(e) => hide(e)} className="hideTicketButton">hide</button>
+          </div>
         </div>
-        <div>
-          <button style={{ display: buttonDisplay }} onClick={(e) => hide(e)} className="hideTicketButton">hide</button>
+        <p className={contentClass}>{props.content}</p>
+        <span style={{ display: props.content.length < 380 && window.innerWidth > 780 ? 'none' : 'inline' }} onClick={(e) => see(e)} className="see">show more...</span>
+        <div className="contact">
+          <div>
+            <p className="email">
+              By&nbsp;
+              <a href={`mailto:${props.userEmail}`}>{props.userEmail}</a>
+              &nbsp;
+              |
+              &nbsp;
+              { myFunction() }
+            </p>
+          </div>
+          <div className="labels">
+            {
+              props.labels !== null && props.labels.map((label) => <div className="label" key={label}>{label}</div>)
+            }
+          </div>
         </div>
       </div>
-      <p className={contentClass}>{props.content}</p>
-      <span style={{display:props.content.length<280 && window.innerWidth>780?"none":"inline"}} onClick={(e) => see(e)} className="see">show more...</span>
-      <div className="contact">
-        <div>
-          <p className="email">
-            By&nbsp;
-            <a href={`mailto:${props.userEmail}`}>{props.userEmail}</a>
-            &nbsp;
-            |
-            &nbsp;
-            { myFunction() }
-          </p>
-        </div>
-        <div className="labels">
-          {
-                    props.labels !== null && props.labels.map((label, index) => 
-                    <div className="label" key={index}>{label}</div>)
-                }
-        </div>
-      </div>
-    </div>
-    <div className="flip" style={{display}}>
-    </div>
+      <div className="flip" style={{ display }} />
     </ReactCardFlip>
   );
 }
