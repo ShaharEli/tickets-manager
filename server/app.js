a8d061acb45a5cbc32ac5e7718cec62de1e9024c
ShaharEli <shahar.e3@gmail.com>
2020-08-27 14:02:19 +0300

    fixed server for tests

--- server/app.js
+++ server/app.js
@@ -1,3 +1,50 @@
-const app = require('./index');
+const fs = require('fs').promises;
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.use(express.static('../client/build'));
+const path = './data.json';
+app.get('/api/tickets', async (req, res) => {
+  const tickets = await fs.readFile(path);
+  const ticketsJson = JSON.parse(tickets);
+  if (!req.query.searchText) {
+    res.send(ticketsJson);
+  } else {
+    const ticketsArray = ticketsJson.filter((item) => item.title.toLowerCase().includes(req.query.searchText.toLowerCase()));
+    res.send(ticketsArray);
+  }
+});
 
+app.post('/api/tickets/:ticketId/done', async (req, res) => {
+  const tickets = await fs.readFile(path);
+  let ticketsJson = JSON.parse(tickets);
+  try {
+    const ticketsArray = ticketsJson.map((item) => {
+      if (item.id === req.params.ticketId) {
+        item.done = true;
+      }
+      return item;
+    });
+    ticketsJson = JSON.stringify(ticketsArray);
+    await fs.writeFile(path, ticketsJson);
+    res.send({ updated: true });
+  } catch (e) { res.send({ updated: false }); }
+});
+
+app.post('/api/tickets/:ticketId/undone', async (req, res) => {
+  const tickets = await fs.readFile(path);
+  let ticketsJson = JSON.parse(tickets);
+  try {
+    const ticketsArray = ticketsJson.map((item) => {
+      if (item.id === req.params.ticketId) {
+        item.done = false;
+      }
+      return item;
+    });
+    ticketsJson = JSON.stringify(ticketsArray);
+    await fs.writeFile(path, ticketsJson);
+    res.send({ updated: true });
+  } catch (e) { res.send({ updated: true }); }
+});
 module.exports = app;
+
--- server/index.js
+++ server/index.js
@@ -1,53 +1,5 @@
-const fs = require('fs').promises;
-const express = require('express');
+const app = require('./app');
 
-const app = express();
 const port = 8080;
-app.use(express.json());
-app.use(express.static('../client/build'));
-const path = './data.json';
-app.get('/api/tickets', async (req, res) => {
-  const tickets = await fs.readFile(path);
-  const ticketsJson = JSON.parse(tickets);
-  if (!req.query.searchText) {
-    res.send(ticketsJson);
-  } else {
-    const ticketsArray = ticketsJson.filter((item) => item.title.toLowerCase().includes(req.query.searchText.toLowerCase()));
-    res.send(ticketsArray);
-  }
-});
 
-app.post('/api/tickets/:ticketId/done', async (req, res) => {
-  const tickets = await fs.readFile(path);
-  let ticketsJson = JSON.parse(tickets);
-  try {
-    const ticketsArray = ticketsJson.map((item) => {
-      if (item.id === req.params.ticketId) {
-        item.done = true;
-      }
-      return item;
-    });
-    ticketsJson = JSON.stringify(ticketsArray);
-    await fs.writeFile(path, ticketsJson);
-    res.send({ updated: true });
-  } catch (e) { res.send({ updated: false }); }
-});
-
-app.post('/api/tickets/:ticketId/undone', async (req, res) => {
-  const tickets = await fs.readFile(path);
-  let ticketsJson = JSON.parse(tickets);
-  try {
-    const ticketsArray = ticketsJson.map((item) => {
-      if (item.id === req.params.ticketId) {
-        item.done = false;
-      }
-      return item;
-    });
-    ticketsJson = JSON.stringify(ticketsArray);
-    await fs.writeFile(path, ticketsJson);
-    res.send({ updated: true });
-  } catch (e) { res.send({ updated: true }); }
-});
-
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
-module.exports = app;
+app.listen(port, console.log(`TIcket manager listening at http://localhost:${port}`));
